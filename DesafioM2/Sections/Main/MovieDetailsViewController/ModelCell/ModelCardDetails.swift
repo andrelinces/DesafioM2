7ea49132c154e88c0cbe1e3a10aa69ede669e86c
Andre Linces <andrelinces@yahoo.com.br>
2022-02-08 18:03:25 -0300

    Created folders of the main view. - creted files models for to modelcells. - File created :   - XIB CardDetailsModelCell   - CardDetailsModelCell   - ModelCardDetails

---
+++ DesafioM2/Sections/Main/MovieDetailsViewController/ModelCell/ModelCardDetails.swift
@@ -0,0 +1,8 @@
+//
+//  ModelCardDetails.swift
+//  DesafioM2
+//
+//  Created by Andre Linces on 08/02/22.
+//
+
+import Foundation
---
+++ DesafioM2/Sections/Main/MovieDetailsViewController/ModelTableView/MovieDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailViewController.swift
+//  DesafioM2
+//
+//  Created by Andre Linces on 08/02/22.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
